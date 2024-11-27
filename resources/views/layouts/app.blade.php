9febcc36014a7eb1eb40a52bbb125ea4aacc99f2
Jesusdjm2 <mirandadjmdjm@gmail.com>
2024-11-27 06:25:50 -0500

    otro mit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hotel Conquistador</title>
+    <title>Hotel Conquistador @yield('title')</title>
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -21,10 +21,10 @@
                 </button>
                 <a href="#" class="text-uppercase text-dark text-decoration-none">ES</a>
                 <a class="navbar-brand text-center fw-bold mx-auto" href="#">
-                    <span style="font-size: 1.8rem; font-family: serif;">Hotel Conquistador</span>
+                    <span style="font-size: 1.8rem; font-family: serif;">Hotel El Conquistador</span>
                 </a>
                 <div class="d-flex align-items-center text-end">
-                    <div class="me-3">
+                    <div class="me-3 d-none d-md-block text-center">
                         <span class="d-block text-success" style="font-size: 0.9rem;">Estancia Climática Positiva</span>
                         <small class="text-muted">Respaldada por Regenera</small>
                     </div>
@@ -72,6 +72,26 @@
     </header>
 
     @yield('content')
+    <footer class="bg-white py-3 shadow-sm">
+        <div
+            class="container d-flex flex-column flex-md-row justify-content-center align-items-center text-center text-md-start">
+            <!-- Logo de Antigua -->
+            <div class="mb-3 mb-md-0">
+                <img src="{{ asset('images/logo-antigua.png') }}" alt="Logo Antigua" class="img-fluid"
+                    style="max-height: 50px;">
+            </div>
+
+            <!-- Separador vertical en pantallas grandes -->
+            <div class="d-none d-md-block mx-3" style="border-left: 1px solid #ccc; height: 40px;"></div>
+
+            <!-- Texto del Copyright -->
+            <div class="text-muted" style="font-size: 0.9rem; line-height: 1.5;">
+                <strong>Copyright © Hotelera del Valle S.A.C.</strong>
+                Todos los derechos de propiedad intelectual en todos los logotipos, diseños, texto, imágenes y otros
+                materiales en este sitio web son propiedad de Hotelera del Valle S.A.C.
+            </div>
+        </div>
+    </footer>
 
     <script>
         const closeMenuBtn = document.querySelector('.close-menu');
